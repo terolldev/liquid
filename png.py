8bd070c3f4da7b8eb0632fae4ff2e3150ce47dbc
TimEiger <100599959+timeigerpy@users.noreply.github.com>
2022-04-01 03:33:53 +0300

    Hot Fix for png.py

--- png.py
+++ png.py
@@ -1,1 +1,1 @@
-info_png = "https://cdn.discordapp.com/attachments/888376246029918228/948743947088461834/777557308258517032.png"
+info_png = "https://cdn.discordapp.com/attachments/888376246029918228/948743947088461834/777557308258517032.png" # info png file bot
